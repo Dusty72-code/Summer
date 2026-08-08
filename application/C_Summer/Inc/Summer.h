9f9a8af1e1ceb053e2d333525d457c64165ec1d9
Dusty <2908251885@qq.com>
2026-08-08 22:57:40 +0800

    Summer

---
+++ application/C_Summer/Inc/Summer.h
@@ -0,0 +1,18 @@
+//
+// Created by Dolores on 2026/8/8.
+//
+
+#ifndef SUMMER_SUMMER_H
+#define SUMMER_SUMMER_H
+
+//烧录云台or底盘
+#define GIMBAL
+//#define CHASSIS
+
+#define CAN_HEARTBEAT_MASK 0x0FU
+#define CAN_SLOW_SEND_PERIOD_MS 100U
+#define WHEEL_MAX_SPEED_RPM 200
+#define STATUS_SELF_TEST 0x08U
+#define STATUS_SYSTEM_OK 0x00U
+
+#endif //SUMMER_SUMMER_H
