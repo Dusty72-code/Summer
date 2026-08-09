38035318d4fe3dbc06356feee2d214183aeea751
Dusty <2908251885@qq.com>
2026-08-10 00:56:53 +0800

    servo_app

---
+++ application/Gimbal/Inc/servo_app.h
@@ -0,0 +1,10 @@
+//
+// Created by Dolores on 2026/8/9.
+//
+
+#ifndef SUMMER_SERVO_APP_H
+#define SUMMER_SERVO_APP_H
+
+void Servo_App_Update(void);
+
+#endif //SUMMER_SERVO_APP_H
---
+++ application/Gimbal/Src/servo_app.c
@@ -0,0 +1,12 @@
+//
+// Created by Dolores on 2026/8/9.
+//
+#include "servo_app.h"
+#include "bsp_servo.h"
+#include "can_app.h"
+
+void Servo_App_Update(void) {
+    GimbalCtrlMsg_t ctrl = CAN_App_GetGimbalCtrl();
+    int8_t srv = (int8_t)((ctrl.servo_target_speed * 100) / SERVO_MAX_SPEED_RPM);
+    BSP_Servo_SetSpeed(srv);
+}
