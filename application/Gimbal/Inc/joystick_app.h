13622bd8183b358787626028d4bdea4268559d4e
Dusty <2908251885@qq.com>
2026-08-09 23:47:50 +0800

    joystick_app

---
+++ application/Gimbal/Inc/joystick_app.h
@@ -0,0 +1,10 @@
+//
+// Created by Dolores on 2026/8/9.
+//
+
+#ifndef SUMMER_JOYSTICK_APP_H
+#define SUMMER_JOYSTICK_APP_H
+
+void Joystick_App_Update(void);
+
+#endif //SUMMER_JOYSTICK_APP_H
---
+++ application/Gimbal/Src/joystick_app.c
@@ -0,0 +1,16 @@
+//
+// Created by Dolores on 2026/8/9.
+//
+#include "joystick_app.h"
+#include "bsp_joystick.h"
+#include "bsp_servo.h"
+#include "can_app.h"
+#include "Summer.h"
+
+void Joystick_App_Update(void) {
+    int8_t x_percent = BSP_Joystick_GetXPercent();
+    int8_t y_percent = BSP_Joystick_GetYPercent();
+    int8_t servo_rpm = (int8_t)((x_percent * (int16_t)SERVO_MAX_SPEED_RPM) / 100);
+    int8_t wheel_rpm = (int8_t)((y_percent * (int16_t)WHEEL_MAX_SPEED_RPM) / 100);
+    CAN_App_SetGimbalCtrl(servo_rpm, wheel_rpm);
+}
