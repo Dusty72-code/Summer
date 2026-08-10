c9cd37280df3665a9a1e7ad7b59a7fc1af31d81f
Dusty <2908251885@qq.com>
2026-08-10 17:41:37 +0800

    joystick_app

--- application/Gimbal/Src/joystick_app.c
+++ application/Gimbal/Src/joystick_app.c
@@ -5,9 +5,13 @@
 #include "bsp_joystick.h"
 #include "bsp_servo.h"
 #include "can_app.h"
+#include "self_test.h"
 #include "Summer.h"
 
 void Joystick_App_Update(void) {
+    if (BSP_Joystick_IsSWPressed() && !SelfTest_IsActive()) {
+        SelfTest_Start();
+    }
     int8_t x_percent = BSP_Joystick_GetXPercent();
     int8_t y_percent = BSP_Joystick_GetYPercent();
     int8_t servo_rpm = (int8_t)((x_percent * (int16_t)SERVO_MAX_SPEED_RPM) / 100);
