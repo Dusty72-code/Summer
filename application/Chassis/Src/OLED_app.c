36f36c367469072c0acfafcc3577b2c0c402a20f
Dusty <2908251885@qq.com>
2026-08-10 17:47:54 +0800

    OLED_app

--- application/Chassis/Src/OLED_app.c
+++ application/Chassis/Src/OLED_app.c
@@ -5,13 +5,18 @@
 #include "bsp_oled.h"
 #include "can_app.h"
 #include "motor_control.h"
+#include "self_test.h"
 #include "Summer.h"
 
 void OLED_app_Update(void) {
     GimbalCtrlMsg_t gimbal_ctrl = CAN_App_GetGimbalCtrl();
     ChassisFeedbackMsg_t chassis_fb = CAN_App_GetChassisFeedback();
     BSP_OLED_Clear();
-    BSP_OLED_ShowString(0, 0, "=== STATUS ===");
+    if (SelfTest_IsActive()) {
+        BSP_OLED_ShowString(0, 0, "SELF TEST");
+    } else {
+        BSP_OLED_ShowString(0, 0, "=== STATUS ===");
+    }
     BSP_OLED_ShowString(0, 1, "SrvT:");
     BSP_OLED_ShowNum(40, 1, gimbal_ctrl.servo_target_speed, 4);
     if (gimbal_ctrl.servo_online) {
