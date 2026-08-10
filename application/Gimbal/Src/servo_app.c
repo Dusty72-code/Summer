4a2b8b21393c44942a0bf6cb6fc6c5cb4e749a3e
Dusty <2908251885@qq.com>
2026-08-10 17:40:59 +0800

    servo_app

--- application/Gimbal/Src/servo_app.c
+++ application/Gimbal/Src/servo_app.c
@@ -4,8 +4,10 @@
 #include "servo_app.h"
 #include "bsp_servo.h"
 #include "can_app.h"
+#include "self_test.h"
 
 void Servo_App_Update(void) {
+    if (SelfTest_IsActive()) return;
     GimbalCtrlMsg_t ctrl = CAN_App_GetGimbalCtrl();
     int8_t srv = (int8_t)((ctrl.servo_target_speed * 100) / SERVO_MAX_SPEED_RPM);
     BSP_Servo_SetSpeed(srv);
